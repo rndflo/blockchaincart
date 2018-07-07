fa7262af7477371eac5db21d03ae5a97654127b7
RND Flo <rndfloinc@gmail.com>
2018-07-06 23:20:04 -0400

    Customized admin

--- blockchaincart/shop/admin.py
+++ blockchaincart/shop/admin.py
@@ -4,7 +4,9 @@ from django.contrib import admin
 from .models import Product
 
 class ProductModelAdmin(admin.ModelAdmin):
-    list_display = ["__str__", "created", "updated"]
+    list_display = ["title", "description", "created", "updated"]
+    list_filter = ["created", "updated"]
+    search_fields = ["title", "description"]
     class Meta:
         model = Product
 
